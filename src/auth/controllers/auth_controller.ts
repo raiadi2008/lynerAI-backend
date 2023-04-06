2b4da9d5c109a718f9f5d3f51826fe27bc75745f
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-04-06 19:24:55 +0530

    formated signup controller

--- src/auth/controllers/auth_controller.ts
+++ src/auth/controllers/auth_controller.ts
@@ -19,28 +19,22 @@ export const signupController = (req: Request, res: Response) => {
   console.log("this reached here bro as well")
   const user_info: SignUpRequestInterface = req.body
 
-  // check if email already exist
-  AuthDB.check(user_info.email)
-
   // create user in firebase
   signup(user_info.email, user_info.password)
     .then((firebase_response: SignUpResponseInterface) => {
       // create user in db
-      AuthDB.create({
+      return AuthDB.create({
         email: user_info.email,
         first_name: user_info.first_name,
         last_name: user_info.last_name,
         _id: firebase_response.user_id,
       } as IUser)
-        .then((db_response: IUser) => {
-          return res.status(HttpStatusCodes.CREATED).json(db_response)
-        })
-        .catch((error) => {
-          const error_code = error.code || HttpStatusCodes.SERVER_ERROR
-          return res.status(error_code).json(error)
-        })
+    })
+    .then((db_response: IUser) => {
+      return res.status(HttpStatusCodes.CREATED).json(db_response)
     })
     .catch((error) => {
-      return res.status(400).json(error)
+      const error_code = error.code || HttpStatusCodes.SERVER_ERROR
+      return res.status(error_code).json(error)
     })
 }
